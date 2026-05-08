02d92ac83d794a323c9c5833ffa2b493bc19cdb7
Kariman Ayman Abd El Latif <karimanaymankaram@gmail.com>
2026-05-09 00:13:51 +0300

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,29 +1,15 @@
 import express from "express";
-import type { Request, Response } from "express";
 import serverless from "serverless-http";
-import cors from "cors";
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
-const PORT = 5000;
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (req, res) => {
   res.json({
-    message: "Hello from Bun + Express + TypeScript",
+    message: "Backend working 🚀",
   });
 });
 
-app.get("/bala7", (req: Request, res: Response) => {
-  res.json({
-    message: "Hello from our bala7 مع تحيات كاري و ضوضو",
-  });
-});
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-//   console.log("Routes: /, /bala7"); // 👈 add this
-// });
-
-export default serverless(app);
+export default app;
+export const handler = serverless(app);
