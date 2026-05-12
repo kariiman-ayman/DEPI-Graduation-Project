756f7f6d59d033225bef58b9f465e8f84656b4c6
Kariman Ayman Abd El Latif <karimanaymankaram@gmail.com>
2026-05-13 00:20:18 +0300

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,9 +7,9 @@ import { adminRoutes, instructorRoutes, studentRoutes } from "./routes";
 const app = express();
 
 const allowedOrigins = [
-  "http://localhost:5174",
-  "http://localhost:5173",
-  "http://localhost:5175",
+  process.env.ADMIN_FRONTEND_URL ?? "http://localhost:5175",
+  process.env.STUDENT_FRONTEND_URL ?? "http://localhost:5173",
+  process.env.INSTRUCTOR_FRONTEND_URL ?? "http://localhost:5174",
 ];
 
 app.use(cors({ origin: allowedOrigins, credentials: true }));
