19e52c71b8791b861cf2eba0a1c07235a6fb6b53
Kariman Ayman Abd El Latif <karimanaymankaram@gmail.com>
2026-05-17 21:56:31 +0300

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,18 @@
 import "dotenv/config";
-// import cors from "cors";
+import cors from "cors";
 import express from "express";
 import serverless from "serverless-http";
 // import { adminRoutes, instructorRoutes, studentRoutes } from "./routes";
 
 const app = express();
 
-// const allowedOrigins = [
-//   process.env.ADMIN_FRONTEND_URL ?? "http://localhost:5175",
-//   process.env.STUDENT_FRONTEND_URL ?? "http://localhost:5173",
-//   process.env.INSTRUCTOR_FRONTEND_URL ?? "http://localhost:5174",
-// ];
+const allowedOrigins = [
+  process.env.ADMIN_FRONTEND_URL ?? "http://localhost:5175",
+  process.env.STUDENT_FRONTEND_URL ?? "http://localhost:5173",
+  process.env.INSTRUCTOR_FRONTEND_URL ?? "http://localhost:5174",
+];
 
-// app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
